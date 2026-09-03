cc62ea4a4d2bf31f1423cb3a92061aafb0158e67
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2026-09-03 16:04:16 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+  reactStrictMode: true,
+  // Memastikan kompresi berjalan di tingkat aplikasi jika CDN platform melewatkannya
+  compress: true,
+  // Mengizinkan konfigurasi eksternal netlify/vercel mendeteksi aset dengan benar
+  poweredByHeader: false,
+  // Membantu optimasi image (jika UI menggunakan komponen Image Next)
+  images: {
+    unoptimized: true, // Optimal untuk deployment statis / serverless multi-platform
+  },
 };
 
 export default nextConfig;
